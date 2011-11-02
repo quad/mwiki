f50c71a7e9c15a254de10db9c4abd1e63c64dca0
Scott Robinson <scott.robinson@thoughtworks.com>
2011-11-02 13:46:40 +0800

    Routing spec for / and pages

---
+++ spec/routing/pages_spec.rb
@@ -0,0 +1,12 @@
+describe 'routing to pages' do
+  it "routes /pages/:title to profile#show for title" do
+    { :get => '/pages/hello' }.should route_to :controller => 'pages',
+                                               :action => 'show',
+                                               :id => 'hello'
+  end
+
+  it "redirects / to /pages" do
+    { :get => '/' }.should route_to :controller => 'pages',
+                                    :action => 'index'
+  end
+end
